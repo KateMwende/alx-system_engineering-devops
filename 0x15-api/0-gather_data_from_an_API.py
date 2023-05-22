43add6ccc0a81d261ad63f17b554c51005a85f5b
KateMwende <kmwende419@gmail.com>
2023-05-22 14:02:38 +0000

    Commenting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# returns information about his/her TODO list progress.
+"""returns information about his/her TODO list progress."""
 import requests
 import sys
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()

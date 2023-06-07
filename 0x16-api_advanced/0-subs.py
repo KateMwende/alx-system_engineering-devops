aad37d85fd7975f44022b50afad462ea3f6556ef
KateMwende <kmwende419@gmail.com>
2023-06-07 11:17:23 +0000

    Counted the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,19 @@ Returns number of subscribers from Reddit API
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-  """
-  Checks number of sucribers in a subreddit
-  """
-  url = 'https://www.reddit.com/r/{}.about.json.format(subreddit)'
-  headers = 
-  response = requests.get(url, headers=headers) 
-  
+    """
+    Checks number of sucribers in a subreddit
+    """
+    url = 'https://www.reddit.com/r/{}.about.json.format(subreddit)'
+    headers = {'User-Agent': 'linux:countSubs:v0.01(by /u/cmm_94)'}
+    response = requests.get(url, headers=headers)
+
+    data = response.json().get('data')
+    count = data.get("subscribers")
+
+    if response.status_code == 200:
+        return (count)
+    else:
+        return (0)

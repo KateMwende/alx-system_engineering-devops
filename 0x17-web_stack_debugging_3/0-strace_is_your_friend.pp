1c69d736ac6c58a34198f3a87a8ad3bc383f1ccb
KateMwende <113633390+KateMwende@users.noreply.github.com>
2023-06-07 23:52:58 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixed php extensions in the wordpress file
 
-exec { 'fix-file':
-  command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
-  path    => '/usr/local/bin/:/bin/'
+file { '/var/www/html/wp-settings.php':
+  ensure  => 'file',
+  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
 }

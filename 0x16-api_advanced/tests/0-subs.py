ae9a3bcf122111e1d331720d9e331d3eb5eb2cfd
KateMwende <kmwende419@gmail.com>
2023-06-07 11:40:34 +0000

    Changed string formatting

--- 0x16-api_advanced/tests/0-subs.py
+++ 0x16-api_advanced/tests/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """
     Checks number of sucribers in a subreddit
     """
-    url = 'https://www.reddit.com/r/{}.about.json.format(subreddit)'
+    url = 'https://www.reddit.com/r/{}.about.json'.format(subreddit)
     headers = {'User-Agent': 'linux:countSubs:v0.01(by /u/cmm_94)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 

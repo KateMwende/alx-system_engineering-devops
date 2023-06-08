ea355587f3890641dfbfbee46b0c0308345c5873
KateMwende <kmwende419@gmail.com>
2023-06-08 20:18:02 +0000

    Deleted file to create again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# fixed php extensions in the wordpress file
-file { '/var/www/html/wp-settings.php':
-  ensure  => 'file',
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
-}
